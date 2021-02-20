7ed8e45d9dc3de834c54bcc623278c8b5b7d1900
Jannatul Ferdous <jannatul.uiu@gmail.com>
2021-02-20 17:35:05 +0100

    Sorted Doubly Linked list with string type elements

--- Java_Linked_List/src/doubly_linked_list/DoublyLinkedList.java
+++ Java_Linked_List/src/doubly_linked_list/DoublyLinkedList.java
@@ -89,7 +89,6 @@ public class DoublyLinkedList {
 	public void addElementAfter(String afterElement, String data) {
 		Node newNode = new Node(data);
 		if (head == null) {
-			System.out.println("List is empty");
 			return;
 		} else if (!hasElement(afterElement)) {
 			System.out.println("Given element is not present in the list");
@@ -110,7 +109,6 @@ public class DoublyLinkedList {
 	public void addElementBefore(String beforeElement, String data) {
 		Node newNode = new Node(data);
 		if (head == null) {
-			System.out.println("List is empty");
 			return;
 		} else if (!hasElement(beforeElement)) {
 			System.out.println("Given element is not present in the list");
@@ -126,6 +124,173 @@ public class DoublyLinkedList {
 				tail = tail.next;
 			}
 		}
+	}
+
+	public void removeFirstNode() {
+		if (head == null) {
+			return;
+		} else {
+			if (head.next == null) {
+				head = null;
+			} else {
+				head.next.previous = null;
+				head = head.next;
+			}
+		}
+	}
+
+	public void removeLastNode() {
+		if (head == null) {
+			return;
+		} else {
+			tail = head;
+			if (head.next == null) {
+				head = null;
+			} else {
+				while (tail.next != null) {
+					tail = tail.next;
+				}
+				tail = tail.previous;
+				tail.next = null;
+			}
+		}
+	}
+
+	public void removeAt(int position) {
+		if (head == null) {
+			return;
+		} else if (position == 0) {
+			removeFirstNode();
+		} else if (position == getSizeList() - 1) {
+			removeLastNode();
+		} else if (position == getSizeList()) {
+			System.out.print("\nThere is no element at this position");
+			return;
+		} else {
+			tail = head;
+			for (int i = 0; i < position; i++) {
+				tail = tail.next;
+			}
+			tail.previous.next = tail.next;
+			tail.next.previous = tail.previous;
+			tail.previous.next = tail.next;
+		}
+	}
+
+	public void removeElement(String data) {
+		if (head == null) {
+			return;
+		} else {
+			if (head.data.equals(data)) {
+				removeFirstNode();
+			} else if (!hasElement(data)) {
+				System.out.print("\nGiven element is not present in the list");
+			} else {
+				tail = head;
+				while (tail != null) {
+					if (tail.next == null && tail.data.equals(data)) {
+						removeLastNode();
+					} else if (tail.data.equals(data)) {
+						tail.previous.next = tail.next;
+						tail.next.previous = tail.previous;
+						tail.previous.next = tail.next;
+					}
+					tail = tail.next;
+				}
+			}
+		}
+	}
+
+	public void removeBeforeElement(String beforeElement) {
+		if (head == null) {
+			return;
+		} else if (head.data.equals(beforeElement)) {
+			System.out.print("\nThere is no element before this to remove");
+		} else if (head.next.data.equals(beforeElement)) {
+			removeFirstNode();
+		} else if (!hasElement(beforeElement)) {
+			System.out.print("\nGiven element is not present in the list");
+		} else {
+			tail = head;
+			while (tail.next != null) {
+				if (tail.next.data.equals(beforeElement)) {
+					tail.previous.next = tail.next;
+					tail.previous = tail.next.previous;
+
+				}
+				tail = tail.next;
+			}
+		}
+	}
+
+	public void displayReversely() {
+		Node previousNode = null;
+		Node nextNode = null;
+		if (head == null) {
+			return;
+		} else if (head.next == null) {
+			return;
+		} else {
+			Node currentNode = head;
+			while (currentNode.next != null) {
+				currentNode = currentNode.next;
+			}
+			head = currentNode;
+			while (currentNode != null) {
+				previousNode = currentNode.previous;
+				currentNode.previous = currentNode.next;
+				currentNode.next = previousNode;
+				currentNode = previousNode;
+			}
+
+		}
+	}
+
+	public void sortedList() {
+		int number_yes = 0;
+		int number_ud = 0;
+		if (head == null) {
+			return;
+		} else if (head.next == null) {
+			return;
+		} else {
+			Node currentNode = head;
+			while (currentNode != null) {
+				if (!currentNode.equals(head)) {
+					if (currentNode.data.contains("Yes")) {
+						number_yes++;
+						if (currentNode.next == null) {
+							removeLastNode();
+						} else {
+							currentNode.previous.next = currentNode.next;
+							currentNode.next.previous = currentNode.previous;
+							currentNode.previous.next = currentNode.next;
+						}
+					} else if (currentNode.data.contains("UD")) {
+						number_ud++;
+						if (currentNode.next == null) {
+							removeLastNode();
+						} else {
+							currentNode.previous.next = currentNode.next;
+							currentNode.next.previous = currentNode.previous;
+							currentNode.previous.next = currentNode.next;
+						}
+					}
+				}
+				currentNode = currentNode.next;
+			}
+			tail = head;
+			if (tail != null) {
+				for (int i = 1; i < number_yes; i++) {
+					addFirst("Yes");
+				}
+				for (int i = 0; i < number_ud; i++) {
+					addLast("UD");
+				}
+				tail = tail.next;
+			}
+		}
+
 	}
 
 	public int getSizeList() {
--- Java_Linked_List/src/doubly_linked_list/TestDoublyLinkedList.java
+++ Java_Linked_List/src/doubly_linked_list/TestDoublyLinkedList.java
@@ -7,12 +7,36 @@ public class TestDoublyLinkedList {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		DoublyLinkedList doublyList = new DoublyLinkedList();
-		doublyList.addFirst("10");
-		doublyList.addFirst("20");
-		doublyList.addLast("30");
-		doublyList.addAt(2, "44");
-		doublyList.addElementAfter("44", "40");
-		doublyList.addElementBefore("44", "25");
+		doublyList.addFirst("No");
+		doublyList.addFirst("Yes");
+		doublyList.addLast("Yes");
+
+		// doublyList.addAt(2, "UD");
+		// doublyList.addElementAfter("Yes", "UD");
+
+		doublyList.addLast("UD");
+		doublyList.addLast("UD");
+		doublyList.addLast("Yes");
+
+		doublyList.addFirst("No");
+		doublyList.addFirst("Yes");
+		doublyList.addLast("Yes");
+		doublyList.addLast("No");
+		doublyList.addLast("Yes");
+		doublyList.addLast("UD");
+		// doublyList.addElementBefore("44", "25");
+		// doublyList.removeFirstNode();
+		// doublyList.removeLastNode();
+		// doublyList.removeAt(2);
+		// doublyList.removeBeforeElement("44");
+		// doublyList.removeElement("2");
+		doublyList.display();
+		/*
+		 * doublyList.displayReversely(); System.out.print("\nList in reverse: ");
+		 * doublyList.display();
+		 */
+		doublyList.sortedList();
+		System.out.print("\nSorted Linked list with string type elements: ");
 		doublyList.display();
 	}
 
